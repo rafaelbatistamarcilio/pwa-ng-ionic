103de59dc543b15d8f73dcfe1bad5430d8cbe16f
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-10-23 22:12:55 -0300

    fontawesome angular library aded

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,25 @@ import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDollarSign, faChevronLeft, faHandHoldingUsd, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(faDollarSign, faChevronLeft, faHandHoldingUsd, faTrashAlt);
+
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   exports: [
     HeaderComponent,
     IonicModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   declarations: [HeaderComponent]
 })
